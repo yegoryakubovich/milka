f60c89bfb0c0063974901c53574f0f611f3993a7
yegoryakubovich <74151311+YegorYakubovich@users.noreply.github.com>
2021-04-19 19:27:48 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect("db.db")
+conn = sqlite3.connect("db.db", check_same_thread=False)
 cursor = conn.cursor()
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS accounts
